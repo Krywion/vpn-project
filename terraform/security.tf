cbfc2cca8ce41ecce50c632805eb2a42d5b50e0c
Krywion <krywionek.maks@gmail.com>
2025-01-01 20:57:39 +0100

    Add VPC and security group resources for VPN setup

---
+++ terraform/security.tf
@@ -0,0 +1,35 @@
+resource "aws_security_group" "vpn" {
+  name_prefix = "${var.project_name}-vpn-"
+  vpc_id      = aws_vpc.main.id
+
+  # OpenVPN
+  ingress {
+    from_port   = 1194
+    to_port     = 1194
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "OpenVPN"
+  }
+
+  # SSH
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "SSH"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all outbound traffic"
+  }
+
+  tags = {
+    Name    = "${var.project_name}-vpn-sg"
+    Project = var.project_name
+  }
+}
