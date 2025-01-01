bd84e8ed3be558fbba5060cc3b72841329a51e9e
Krywion <krywionek.maks@gmail.com>
2025-01-01 21:03:50 +0100

    Add Terraform configuration for AWS provider and S3 backend

---
+++ terraform/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">= 1.0.0"
+  
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  
+  backend "s3" {
+    bucket = "my-terraform-state"
+    key    = "vpn/terraform.tfstate"
+    region = "eu-central-1"
+  }
+}
